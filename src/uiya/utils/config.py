d77b2432cbe4dff35e4f5d7d3a3a2e2507e926fa
MrXnneHang <XnneHang@gmail.com>
2025-03-06 15:19:32 +0800

    :sparkles: feature: Search the config file automatically from HOME_DIR and WORK_DIR.
    Former-commit-id: 5c7d51f78c4b18eb65465a86e6e5a974bb1200c4

---
+++ src/uiya/utils/config.py
@@ -0,0 +1,41 @@
+from __future__ import annotations
+
+from pathlib import Path
+
+import yaml
+import os
+import platform
+
+
+def xdg_config_home() -> Path:
+    if (env := os.environ.get("XDG_CONFIG_HOME")) and (path := Path(env)).is_absolute():
+        return path
+    home = Path.home()
+    if platform.system() == "Windows":
+        return home / "AppData"
+    return home / ".config"
+
+
+def search_for_settings_file() -> Path | None:
+    settings_file = Path("ACGO.yaml")
+    if not settings_file.exists():
+        settings_file = xdg_config_home() / "ACGO.yaml"
+    if not settings_file.exists():
+        return None
+    return settings_file
+
+
+# 读取配置文件
+def load_config() -> dict[str, bool | str]:
+    """读取配置文件到字典"""
+    path = search_for_settings_file()
+    try:
+        if path is None:
+            raise FileNotFoundError("配置文件不存在")
+        else:
+            with path.open(encoding="utf-8") as f:
+                return yaml.load(f, Loader=yaml.FullLoader)
+    except FileNotFoundError:
+        # 打印错误信息
+        print(f"配置文件 {path} 不存在")
+        return {}
