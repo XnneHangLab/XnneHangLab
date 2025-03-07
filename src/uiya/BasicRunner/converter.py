3a8e68ecf1f1ef02796eaeef9b8dd90e611a6f21
MrXnneHang <XnneHang@gmail.com>
2025-03-07 17:41:59 +0800

    :sparkles: feature: Convert AutoModelResponse to Senetences.
    Former-commit-id: 34b064d25ef5ee9a78554eb7fd75831103b28dae

---
+++ src/uiya/BasicRunner/converter.py
@@ -0,0 +1,121 @@
+# -*- coding: utf-8 -*-
+
+import re
+
+from uiya._dataclass import ConfigParser
+from uiya._typing import AutoModelResponse, Sentence
+
+# 对于 Response 中存在的英文单词的处理方式。
+# text: 这个软件基于Funasr。(7 words),(12 characters, without punc)
+# ts_list: [(0, 100), (100, 200), (200, 300), (300, 400), (400, 500), (500, 600), (600, 700)]
+# text_seg: "这 个 软 件 基 于 F u n a s r"
+# 坚持的原则是， text 的 word_num = ts_list 的长度，不管这个单词是不是有效的单词。
+# TODO: 把 text_seg 的英文单词也合并起来。
+
+
+def segment_text(text: str):
+    """
+    将文本按照标点符号分割成句子列表
+    """
+
+    Config = ConfigParser()
+    pop_list = Config.punctuation_list
+
+    # 移除 pop_list 中的单引号
+    # 应对这种情况, He's a boy. 我希望`He's`被视作一个单词
+    pop_list_without_single_quote = pop_list.replace("'", "")
+
+    # 使用负向先行断言和负向后行断言确保单引号左右同时为字母时不分割
+    pattern = f"(?<![a-zA-Z])'|'(?![a-zA-Z])|([{pop_list_without_single_quote}])"
+    sentences = re.split(pattern, text)
+    sentences = [s for s in sentences if s.strip()]  # 去除空白元素
+    return sentences
+
+
+def split_into_words(text: str) -> list[str]:
+    """
+    将句子分割成单个汉字和单词，保留标点符号
+    Example:
+    print(split_into_words("晚安纳尼南尼nony!")) # ['晚', '安', '纳', '尼', '南', '尼', 'nony', '!']
+    print(split_into_words("就的真的妈a等等?")) # ['就', '的', '真', '的', '妈', 'a', '等', '等', '?']
+    print(split_into_words("多喜天dustin birthday.")) # ['多', '喜', '天', 'dustin', 'birthday', '.']
+    print(split_into_words("He's 我见过的。")) # ["He's", '我', '见', '过', '的', '。']
+    """
+
+    # 正则表达式用于匹配英文单词、汉字、标点符号和英文缩写
+    pattern = re.compile(
+        r"[a-zA-Z]+(?:'[a-zA-Z]+)?|[\u4e00-\u9fa5]|[^\u4e00-\u9fa5a-zA-Z\s]"
+    )
+
+    # 使用 findall 方法找到所有匹配的部分
+    words = pattern.findall(text)
+
+    return words
+
+
+def match_timestamps_to_words(
+    text: str, timestamps: list[list[int]]
+) -> list[list[int]]:
+    """
+    将时间戳分配给对应的单词,同时去除标点符号。
+    """
+    Config = ConfigParser()
+    pop_list = Config.punctuation_list
+    words: list[str] = split_into_words(text)
+    matched: list[list[int]] = []
+    ts_idx = 0
+
+    for word in words:
+        if word in pop_list:
+            continue
+        start, end = timestamps[ts_idx]
+        matched.append([start, end])  # 有多少个单词，就匹配多少组 start end.
+        ts_idx += 1
+
+    return matched
+
+
+def calculate_length(segmented_text: str) -> int:
+    """
+    计算分割后单词和汉字的长度
+    """
+    words = split_into_words(segmented_text)
+    length = 0
+    for word in words:
+        if word not in ConfigParser().punctuation_list:
+            length += 1
+    return length
+
+
+def convert_response_to_sentences(input_data: AutoModelResponse) -> list[Sentence]:
+    Config = ConfigParser()
+    pop_list = Config.punctuation_list
+
+    results: list[Sentence] = []
+    text = input_data["text"]
+    timestamps = input_data["timestamp"]
+
+    sentences = segment_text(text)
+    current_ts_idx = 0
+    for sentence in sentences:
+        if sentence in pop_list:
+            continue
+        else:
+            ts_list = timestamps[current_ts_idx : current_ts_idx + len(sentence)]
+            text_seg = " ".join(sentence)
+            start = ts_list[0][0]
+            end = ts_list[-1][1]
+            matched_ts_list = match_timestamps_to_words(sentence, ts_list)
+
+            result_item: Sentence = {
+                "text": sentence,
+                "start": start,
+                "end": end,
+                "text_seg": text_seg,
+                "ts_list": matched_ts_list,
+            }
+
+            results.append(result_item)
+            current_ts_idx += calculate_length(sentence)
+
+    return results
--- src/uiya/test.py
+++ src/uiya/test.py
@@ -1,11 +1,8 @@
 import funasr
 import torch
 import torchaudio
-import argparse
-from pathlib import Path
 
-from uiya.utils.model import FunASRModel, generate_results
-from uiya.BasicRunner.extractor import save_only_text_from_response
+from uiya.BasicRunner.converter import split_into_words
 
 
 def main():
@@ -13,15 +10,31 @@ def main():
     print(f"torch:{torch.__version__}")
     print(f"torchaudio:{torchaudio.__version__}")
 
-    parser = argparse.ArgumentParser(description="将wav音频转换成srt")
-    parser.add_argument(
-        "-i", "--input_path", default="./example.wav", help="输入音频文件"
-    )
-    args = parser.parse_args()
+    # === test save_only_text_from_response ===
+    # parser = argparse.ArgumentParser(description="将wav音频转换成srt")
+    # parser.add_argument(
+    #     "-i", "--input_path", default="./example.wav", help="输入音频文件"
+    # )
+    # args = parser.parse_args()
 
-    Model = FunASRModel()
-    model = Model.full_version()
-    response = generate_results(
-        model=model, input_path=Path(args.input_path), hot_word="", debug=True
-    )
-    save_only_text_from_response(response=response, output_dir=Path("./test"))
+    # Model = FunASRModel()
+    # model = Model.full_version()
+    # response = generate_results(
+    #     model=model, input_path=Path(args.input_path), hot_word="", debug=True
+    # )
+    # save_only_text_from_response(response=response, output_dir=Path("./test"))
+
+    # === test convert_response_to_sentences ===
+
+    # === test split_into_words ===
+    # print(convert_format(input_data,debug=True))
+    print(
+        split_into_words("晚安纳尼南尼nony!")
+    )  # ['晚', '安', '纳', '尼', '南', '尼', 'nony']
+    print(
+        split_into_words("就的真的妈a等等?")
+    )  # ['就', '的', '真', '的', '妈', 'a', '等', '等']
+    print(
+        split_into_words("多喜天dustin birthday.")
+    )  # ['多', '喜', '天', 'dustin', 'birthday']
+    print(split_into_words("He's 我见过的。"))  # ["He's", '我', '见', '过', '的', '。']
