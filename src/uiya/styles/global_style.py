f97498d0ba1aa6f6cccda78a260ae3852fce6e98
MrXnneHang <XnneHang@gmail.com>
2025-03-31 13:12:21 +0800

    :bug: fix: change txt -> url.
    Former-commit-id: e9ec1e8a21250417218c2eaf480201ba70ad1be3

--- src/uiya/styles/global_style.py
+++ src/uiya/styles/global_style.py
@@ -9,7 +9,7 @@ def style(home: bool = False):  # 定义应用自定义字体的函数
             page_title="Auto-Caption-Generator-Offline",
             layout="wide",
             menu_items={
-                "Get Help": "还没有噢",
+                "Get Help": "https://lab.xnnehang.top/audio",
                 "Report a bug": "https://github.com/MrXnneHang/MrXnneHang/issues",
                 "About": "#### @2024 XnneHang 版权所有",
             },
