f557d2488f514e4c57a1f01a61de43816d3c97e7
MrXnneHang <XnneHang@gmail.com>
2025-06-29 12:10:06 +0800

    add bert-vits as submodule

--- run_server.py
+++ run_server.py
@@ -1,27 +1,15 @@
 from __future__ import annotations
 
 import argparse
-import atexit
-import gc
 import os
 import sys
-from contextlib import asynccontextmanager
 from pathlib import Path
 
 import tomli
-import torch
 import uvicorn
-from fastapi import FastAPI
 from loguru import logger
-from vits import utils
-from vits.config import config
-from vits.infer import get_net_g, latest_version
-from vits.state_manager import tts_state_manager
 
-from src.lab.api.core_logic import load_model
 from src.lab.config_manager.vtuber import Config, read_yaml, validate_config
-
-# from upgrade import sync_user_config, select_language
 from src.lab.server import WebSocketServer
 
 os.environ["HF_HOME"] = str(Path(__file__).parent / "models")
@@ -59,7 +47,6 @@ def init_logger(console_log_level: str = "INFO") -> None:
 def parse_args():
     parser = argparse.ArgumentParser(description="Open-LLM-VTuber Server")
     parser.add_argument("--verbose", action="store_true", help="Enable verbose logging")
-    parser.add_argument("--hf_mirror", action="store_true", help="Use Hugging Face mirror")
     return parser.parse_args()
 
 
@@ -88,12 +75,4 @@ def run(console_log_level: str):
 if __name__ == "__main__":
     args = parse_args()
     console_log_level = "DEBUG" if args.verbose else "INFO"
-    # if args.verbose:
-    #     logger.info("Running in verbose mode")
-    # else:
-    #     logger.info(
-    #         "Running in standard mode. For detailed debug logs, use: uv run run_server.py --verbose"
-    #     )
-    if args.hf_mirror:
-        os.environ["HF_ENDPOINT"] = "https://hf-mirror.com"
     run(console_log_level=console_log_level)
