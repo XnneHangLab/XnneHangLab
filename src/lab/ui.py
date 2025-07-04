789dc2bf680845c58023ed37c0a918e326bb3853
MrXnneHang <XnneHang@gmail.com>
2025-07-04 08:35:27 +0800

    将 bert_vits 的包和其余主项目解耦.即使不导入,不存在依赖也可以允许其余项目.

--- src/lab/ui.py
+++ src/lab/ui.py
@@ -6,6 +6,7 @@ from pathlib import Path
 import streamlit as st
 
 from lab.config_manager import RootAbsDir, load_settings_file
+from lab.config_manager.package import packages
 from lab.styles.global_style import style
 
 style(True)
@@ -22,10 +23,13 @@ def main():
         "home": ROOT_DIR / "src" / "lab" / "pages" / "project" / "home.py",
         "audio": ROOT_DIR / "src" / "lab" / "pages" / "project" / "audio.py",
         "settings": ROOT_DIR / "src" / "lab" / "pages" / "setting" / "set.py",
-        "todo": ROOT_DIR / "packages" / "todo" / "src" / "todo" / "streamlit_to_do.py",
-        "uiya": ROOT_DIR / "packages" / "yutto-uiya" / "src" / "uiya" / "yutto_uiya.py",
-        "bert-vits": ROOT_DIR / "packages" / "bert-vits" / "src" / "vits" / "bert_vits.py",
     }
+    if packages["bert_vits"]:
+        PAGE_PATHS["bert-vits"] = ROOT_DIR / "packages" / "bert-vits" / "src" / "vits" / "bert_vits.py"
+    if packages["to_do_list"]:
+        PAGE_PATHS["todo"] = ROOT_DIR / "src" / "lab" / "pages" / "project" / "todo.py"
+    if packages["yutto_uiya"]:
+        PAGE_PATHS["uiya"] = ROOT_DIR / "src" / "lab" / "pages" / "project" / "uiya.py"
     # 检查路径是否存在
     for name, path in PAGE_PATHS.items():
         if not path.exists():
@@ -43,27 +47,36 @@ def main():
         ],
         "Project": [
             st.Page(
-                page=str(PAGE_PATHS["todo"]),
-                title="待办事项/Roadmap",
-                icon=":material/checklist:",
-            ),
-            st.Page(
                 page=str(PAGE_PATHS["audio"]),
                 title="音频识别",
                 icon=":material/headset:",
             ),
+        ],
+    }
+    if packages["bert_vits"]:
+        pages["Project"].append(
             st.Page(
-                page=str(PAGE_PATHS["uiya"]),
-                title="b站视频下载",
-                icon=":material/graphic_eq:",
-            ),
-            st.Page(  # TODO: 分离 UI 和其他代码。另外，在开启时初始化模型
                 page=str(PAGE_PATHS["bert-vits"]),
                 title="BERT-VITS",
                 icon=":material/robot:",
             ),
-        ],
-    }
+        )
+    if packages["to_do_list"]:
+        pages["Project"].append(
+            st.Page(
+                page=str(PAGE_PATHS["todo"]),
+                title="待办事项/Roadmap",
+                icon=":material/checklist:",
+            )
+        )
+    if packages["yutto_uiya"]:
+        pages["Project"].append(
+            st.Page(
+                page=str(PAGE_PATHS["uiya"]),
+                title="b站视频下载",
+                icon=":material/graphic_eq:",
+            )
+        )
     pg = st.navigation(pages, position="sidebar")
     pg.run()
 
